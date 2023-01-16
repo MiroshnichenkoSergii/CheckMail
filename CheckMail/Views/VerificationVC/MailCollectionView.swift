0c577d5b3f5cc56b3b59e15ff8746e6c97fb7215
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-16 14:58:38 +0200

    Add selectMailDelegate to MailCollectionView

---
+++ CheckMail/Views/VerificationVC/MailCollectionView.swift
@@ -0,0 +1,53 @@
+//
+//  MailCollectionView.swift
+//  CheckMail
+//
+//  Created by Sergii Miroshnichenko on 16.01.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol SelectProposedMailProtocol: AnyObject {
+    func selectProposedMail(indexPath: IndexPath)
+}
+
+enum idCell: String {
+    case idMailCell
+}
+
+class MailCollectionView: UICollectionView {
+    
+    weak var selectMailDelegate: SelectProposedMailProtocol?
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        configure()
+        
+        register(MailCollectionViewCell.self, forCellWithReuseIdentifier: idCell.idMailCell.rawValue)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .none
+    }
+}
+
+//MARK: - Collection View Delegate
+
+extension MailCollectionView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectMailDelegate?.selectProposedMail(indexPath: indexPath)
+    }
+}
+
+extension MailCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width / 2, height: 40)
+    }
+}
