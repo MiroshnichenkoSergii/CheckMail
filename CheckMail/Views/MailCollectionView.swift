5b2bd9d0b3b49ceabcc8bdd09afda6d5d01e32a4
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-16 14:10:01 +0200

    Add MailCollectionView class

--- CheckMail/Views/MailCollectionView.swift
+++ CheckMail/Views/MailCollectionView.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+enum idCell: String {
+    case idMailCell
+}
+
+class MailCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        configure()
+        
+        register(UICollectionViewCell.self, forCellWithReuseIdentifier: idCell.idMailCell.rawValue)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .none
+    }
+}
