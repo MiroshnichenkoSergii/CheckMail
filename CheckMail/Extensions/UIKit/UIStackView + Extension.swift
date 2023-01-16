c20a35425617902f0ec86635b6707459b8dde13c
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-16 14:23:48 +0200

    Relocate view files to VerificationVC

--- CheckMail/Extensions/UIKit/UIStackView + Extension.swift
+++ CheckMail/Extensions/UIKit/UIStackView + Extension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
