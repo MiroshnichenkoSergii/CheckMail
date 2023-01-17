6324e7bd2414e1c2b643bf8912ae1b485b713bbf
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-17 18:50:46 +0200

    Add String extension

--- CheckMail/Extensions/String + Extensions.swift
+++ CheckMail/Extensions/String + Extensions.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension String {
+    
+    func isValid() -> Bool {
+        let format = "SELF MATCHES %@"
+        let regularExpression = "[a-zA-Z0-9._]+@[a-zA-Z]\\.[a-zA-Z]{2,}"
+        
+        return NSPredicate(format: format, regularExpression).evaluate(with: self)
+    }
+    
+}
