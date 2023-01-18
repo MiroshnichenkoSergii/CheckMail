9def1c5309d39ab2eb2fe32e8a8cd8b30f45ce42
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-18 15:45:13 +0200

    Add Alert struct

--- CheckMail/Views/Alerts.swift
+++ CheckMail/Views/Alerts.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+struct Alert {
+    
+    private static func showSimpleAlert(vc: UIViewController, title: String, message: String) {
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        DispatchQueue.main.async {
+            vc.present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    static func showResultAlert(vc: UIViewController, message: String) {
+        showSimpleAlert(vc: vc, title: "Result", message: message)
+    }
+    
+    static func showErrorAlert(vc: UIViewController, message: String, completion: @escaping() -> Void) {
+        showSimpleAlert(vc: vc, title: "Error", message: message)
+    }
+}
