d91ca3771e8cc5fabe6a0be3b500f2e214f78f5f
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2023-01-18 14:47:54 +0200

    Add NetworkRequest class and apiKey (from: https://app.kickbox.com/accounts/FGU5N4ij/verify/api)

---
+++ CheckMail/Network/MailApiKey.swift
@@ -0,0 +1,10 @@
+//
+//  MailApiKey.swift
+//  CheckMail
+//
+//  Created by Sergii Miroshnichenko on 18.01.2023.
+//
+
+import Foundation
+
+let apiKey = "live_12f4a3148a79430561bc351fdb0bc3e0534524eedc54153ced68dc0532bb04fe"
